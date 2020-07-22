79ef24887af87a9ea36aef20af1d491bbba271ad
Michael "Radar" Raeder <mcraeder@gmail.com>
2020-07-22 18:19:23 -0500

    Update block-russia.sh
    URL and stuff

--- block-russia.sh
+++ block-russia.sh
@@ -6,11 +6,11 @@ ipset -N russia hash:net
 # remove any old list that might exist from previous runs of this script
 rm /etc/uzone
 
-# Pull the latest IP set for China
+# Pull the latest IP set for Russia
 cd /etc
 wget -P . http://www.ipdeny.com/ipblocks/data/countries/ru.zone
 
-# Add each IP address from the downloaded list into the ipset 'china'
+# Add each IP address from the downloaded list into the ipset 'russia'
 for i in $(cat /etc/ru.zone ); do ipset -A russia $i; done
 
 # Restore iptables
